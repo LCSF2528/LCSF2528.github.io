cbc9d65565fa18dd9010ab9d06b0b2b2db780494
Billygotclout <williamsoluwademilade@gmail.com>
2025-12-01 12:08:07 -0500

    second commit

---
+++ scripts/main.js
@@ -0,0 +1,10 @@
+function toggleMenu() {
+  var menu = document.getElementById("mobileMenu");
+
+  if (menu.style.display === "flex") {
+    menu.style.display = "none";
+  } else {
+    menu.style.display = "flex";
+    menu.style.flexDirection = "column";
+  }
+}
